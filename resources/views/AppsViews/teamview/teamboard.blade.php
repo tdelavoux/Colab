d161a0d51353e4903233f3249b9ab179a038e2a2
Delavoux <thibault.delavoux@gmail.com>
2020-12-02 22:09:36 +0100

    DEV FRONT - TEAM
    Ajout de la page de gestion des teams

--- resources/views/AppsViews/teamview/teamboard.blade.php
+++ resources/views/AppsViews/teamview/teamboard.blade.php
@@ -1,21 +1,21 @@
 @extends('include.layout')
 
-@section('title_page', 'Scrum Board')
+@section('title_page', 'Teams Board')
 
 @section('content')
 <div id="content-page">
     <h1>Projet 1 / Sous Projet 1 / Tableau</h1>
 
-    <button class="btn btn-success">Ajouter une catégorie</button>
-    <button class="btn btn-primary">Ouvrir un ticket</button>
+    <button class="btn btn-success">Ajouter une equipe</button>
 
     <hr>
 
-    <div class="row">
+    <div class="content-page-inline option-content-page">
 
-     
+        @include('AppsViews.teamview.TeamPartials._optionMenu')
+        @include('AppsViews.teamview.TeamPartials._optionContentInfos')
+        
     </div>
 
 </div>
-
 @stop
--- resources/views/include/partials/_navLeft.blade.php
+++ resources/views/include/partials/_navLeft.blade.php
@@ -8,6 +8,6 @@
     <a class="nav-link-left" href="{{route('bugsView',1) }}"><i class="fas fa-bug"></i></a>
     <a class="nav-link-left" href="#"><i class="fas fa-comment-dots"></i></a>
     <a class="nav-link-left" href="#"><i class="fas fa-chart-bar"></i></a>
-    <a class="nav-link-left" href="#"><i class="fas fa-users"></i></a>
+    <a class="nav-link-left" href="{{route('teamView',1) }}"><i class="fas fa-users"></i></a>
     <a class="nav-link-left" href="#"><i class="fas fa-cogs"></i></a>
 </div>
