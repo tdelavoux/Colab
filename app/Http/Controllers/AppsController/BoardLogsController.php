d21d1ac4b516132ca6704cb00f58ce51b1609c0c
Delavoux <thibault.delavoux@gmail.com>
2020-12-03 16:23:21 +0100

    DEV FRONT - Logs
    Adding Front page of boards

--- app/Http/Controllers/AppsController/BoardLogsController.php
+++ app/Http/Controllers/AppsController/BoardLogsController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers\AppsController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class BoardChatController extends Controller
+class BoardLogsController extends Controller
 {
     public function execute($fkBoard)
     {
-        return view('AppsViews.chatView.chatboard');
+        return view('AppsViews.logsView.logsboard');
     }
 }
--- resources/views/AppsViews/logsview/logsboard.blade.php
+++ resources/views/AppsViews/logsview/logsboard.blade.php
@@ -9,96 +9,31 @@
     <hr>
 
     <div class="container">
+        <div class="row team-user-bloc">
 
-        <!---------------- ZONE DE POST ----------------------------->
-        <div class="row">
-            <div class="chat-express-bloc jumbotron col-md-12">
-                <textarea class="form-control" placeholder="Partager une pensée, une question, un document"></textarea>
-                <div class="btn-line-right">
-                    <button class="btn btn-primary btn-sm mt-05"><i class="far fa-paper-plane"></i>Partager</button>
+            <div class="col-md-12 team-user-bloc-line logs-user-bloc-line">
+                <div class="rounded-circle profile-img-sm" style="background-image:url('{{asset('/img/user/3.jpg')}}');background-position: center;background-repeat: no-repeat;background-size: cover;"></div>
+                <div class="team-user-infos logs-infos">
+                    <span class="team-user-name chat-answer-user-name">Thibault Delavoux</span>
+                    <span>A ajouté un commentaire à la tâche <a class="log-links" href="#">#125 Ajouter des fichiers de logs au projet</a></span>
+                </div>
+                <div>
+                    <span class="chat-card-timer"><i class="far fa-clock"></i>10 minutes ago</span>
                 </div>
             </div>
-        </div>
-        <!---------------- FIN ZONE DE POST ------------------------->
-    
-        <!---------------- ZONE DE CHAT ----------------------------->
-        <div class="col-md-12">
-            <div class="card chat-card ">
-                <div class="card-header chat-header-card">
-                    <div class="user-chating">
-                        <div class="rounded-circle profile-img-xs" style="background-image:url('{{asset('/img/user/3.jpg')}}');background-position: center;background-repeat: no-repeat;background-size: cover;"></div>
-                        <span class="team-user-name">Thibault Delavoux</span>
-                    </div>
-                    <div class="chat-card-timer"><i class="far fa-clock"></i>21/11/2020</div>
+
+            <div class="col-md-12 team-user-bloc-line logs-user-bloc-line">
+                <div class="rounded-circle profile-img-sm" style="background-image:url('{{asset('/img/user/3.jpg')}}');background-position: center;background-repeat: no-repeat;background-size: cover;"></div>
+                <div class="team-user-infos logs-infos">
+                    <span class="team-user-name chat-answer-user-name">Thibault Delavoux</span>
+                    <span>A changé de statur de la tâche <a class="log-links" href="#">#125 Ajouter des fichiers de logs au projet</a> à <span class="log-value">Cloturé</span></span>
                 </div>
-
-                <div class="card-body">
-                    <div class="card-body-media">
-                        <div class="chat-media-btn-line">
-                            <button class="btn btn-link btn-media-link"><i class="fas fa-thumbtack"></i>Epingler</button>
-                        </div> 
-                    </div>
-                    <span class="card-text">Salut les asticots. y en a qui savent comment on fait ? :)</span>
-
-                    <hr>
-
-                    <div class="card-body-media-answer-line">
-                        <div class="rounded-circle profile-img-xs" style="background-image:url('{{asset('/img/user/3.jpg')}}');background-position: center;background-repeat: no-repeat;background-size: cover;"></div>
-                        <div class="card-body-media-answer-line-text">
-                            <span class="team-user-name chat-answer-user-name">Thibault Delavoux</span>
-                            <div class="">Nan laissez tombé j'ai trouvé.</div>
-                        </div>
-                    </div>
-
-                    <div class="card-body-media-answer-line">
-                        <div class="rounded-circle profile-img-xs" style="background-image:url('{{asset('/img/user/3.jpg')}}');background-position: center;background-repeat: no-repeat;background-size: cover;"></div>
-                        <div class="card-body-media-answer-line-text">
-                            <span class="team-user-name chat-answer-user-name">Thibault Delavoux</span>
-                            <div class="">C'était simple finalement :p.</div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="card-footer">
-                    <textarea class="form-control" placeholder="Une réaction ? "></textarea>
-                    <div class="btn-line-right">
-                        <button class="btn btn-sm btn-info mt-05">Répondre</button>
-                    </div>
+                <div>
+                    <span class="chat-card-timer"><i class="far fa-clock"></i>10 minutes ago</span>
                 </div>
             </div>
+
         </div>
-
-        <div class="col-md-12">
-            <div class="card chat-card ">
-                <div class="card-header chat-header-card">
-                    <div class="user-chating">
-                        <div class="rounded-circle profile-img-xs" style="background-image:url('{{asset('/img/user/3.jpg')}}');background-position: center;background-repeat: no-repeat;background-size: cover;"></div>
-                        <span class="team-user-name">Thibault Delavoux</span>
-                    </div>
-                    <div class="chat-card-timer"><i class="far fa-clock"></i>21/11/2020</div>
-                </div>
-
-                <div class="card-body">
-                    <div class="card-body-media">
-                        <div class="chat-media-btn-line">
-                            <button class="btn btn-link btn-media-link"><i class="fas fa-thumbtack"></i>Epingler</button>
-                        </div> 
-                    </div>
-                    <span class="card-text"><img src="{{asset('/img/test.jpg')}}" class="img-thumbnail" /></span>
-
-                </div>
-
-                <div class="card-footer">
-                    <textarea class="form-control" placeholder="Une réaction ? "></textarea>
-                    <div class="btn-line-right">
-                        <button class="btn btn-sm btn-info mt-05">Répondre</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!---------------- ZONE DE CHAT ----------------------------->
-
     </div>
 </div>
 @stop
