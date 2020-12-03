fb8e052cf866d22315b89e82109254d6c523a6b2
Delavoux <thibault.delavoux@gmail.com>
2020-12-03 16:40:16 +0100

    UX DESIGN - Tooltips
    adding tooltips to left nav bar

--- resources/views/include/partials/_navLeft.blade.php
+++ resources/views/include/partials/_navLeft.blade.php
@@ -1,13 +1,13 @@
 <!-- Side navigation -->
 <div class="sidenav">
-    <a class="nav-link-left" href="#"><i class="fas fa-book-reader"></i></a>
-    <a class="nav-link-left" href="#"><i class="fas fa-eye"></i></a>
-    <a class="nav-link-left" href="{{route('logsView',1) }}"><i class="fas fa-bars"></i></a>
-    <a class="nav-link-left" href="{{route('scrumView',1) }}"><i class="far fa-window-maximize"></i></a>
-    <a class="nav-link-left" href="{{route('kabanView',1) }}"><i class="fab fa-trello"></i></a>
-    <a class="nav-link-left" href="{{route('bugsView',1) }}"><i class="fas fa-bug"></i></a>
-    <a class="nav-link-left" href="{{route('chatView',1) }}"><i class="fas fa-comment-dots"></i></a>
-    <a class="nav-link-left" href="#"><i class="fas fa-chart-bar"></i></a>
-    <a class="nav-link-left" href="{{route('teamView',1) }}"><i class="fas fa-users"></i></a>
-    <a class="nav-link-left" href="#"><i class="fas fa-cogs"></i></a>
+    <a class="nav-link-left left-bar-tooltips" data-toggle="tooltip" title="Disabled" href="#"><i class="fas fa-book-reader"></i></a>
+    <a class="nav-link-left left-bar-tooltips" data-toggle="tooltip" title="Disabled" href="#"><i class="fas fa-eye"></i></a>
+    <a class="nav-link-left left-bar-tooltips" data-toggle="tooltip" title="Historique" href="{{route('logsView',1) }}"><i class="fas fa-bars"></i></a>
+    <a class="nav-link-left left-bar-tooltips" data-toggle="tooltip" title="Gestionnaire Scrum" href="{{route('scrumView',1) }}"><i class="far fa-window-maximize"></i></a>
+    <a class="nav-link-left left-bar-tooltips" data-toggle="tooltip" title="Gestionnaire Kaban" href="{{route('kabanView',1) }}"><i class="fab fa-trello"></i></a>
+    <a class="nav-link-left left-bar-tooltips" data-toggle="tooltip" title="Issues" href="{{route('bugsView',1) }}"><i class="fas fa-bug"></i></a>
+    <a class="nav-link-left left-bar-tooltips" data-toggle="tooltip" title="Chat" href="{{route('chatView',1) }}"><i class="fas fa-comment-dots"></i></a>
+    <a class="nav-link-left left-bar-tooltips" data-toggle="tooltip" title="Statistiques" href="#"><i class="fas fa-chart-bar"></i></a>
+    <a class="nav-link-left left-bar-tooltips" data-toggle="tooltip" title="Equipes" href="{{route('teamView',1) }}"><i class="fas fa-users"></i></a>
+    <a class="nav-link-left left-bar-tooltips" data-toggle="tooltip" title="Paramètres" href="#"><i class="fas fa-cogs"></i></a>
 </div>
