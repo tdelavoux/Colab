2a05e0a9dad4896198821af42aac228b01bd7334
Delavoux <thibault.delavoux@gmail.com>
2020-12-09 18:28:57 +0100

    DEV FRONT - Board Params 3
    habilitations

--- resources/views/AppsViews/paramsview/ParamsPartials/_paramsAccess.blade.php
+++ resources/views/AppsViews/paramsview/ParamsPartials/_paramsAccess.blade.php
@@ -10,68 +10,268 @@
 <div class="option-page-body">
     <h2>Habilitations</h2>
 
+    <!-- --------------- LOGS ------------------------->
     <div class="habilitation-bloc col-md-12">
         <div class="habilitation-line">
-            <div class="habilitation-bloc-header collapsable" data-toggle="collapse" data-target="#habs-Logs">
-                Logs
+            <div class="habilitation-bloc-header" data-toggle="collapse" data-target="#habs-Logs" >
+                <span><i class="fas fa-bars"></i>  Logs</span>
+                <span class="collapse-arrrow" ><i class="fas fa-chevron-right"></i></span>
             </div>
-            <div class="habilitation-bloc-body collapse" id="habs-Logs">
-                test
+            <div class="collapse habilitation-bloc-body" id="habs-Logs">
+                <div class="habilitation-body-line">
+                    <span>Accès au contenu</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
+    <!-- --------------- SCRUM ------------------------->
     <div class="habilitation-bloc col-md-12">
         <div class="habilitation-line">
             <div class="habilitation-bloc-header collapsable" data-toggle="collapse" data-target="#habs-Scrum">
-                Scrum
+                <span><i class="far fa-window-maximize"></i>  Scrum</span>
+                <span class="collapse-arrrow" ><i class="fas fa-chevron-right"></i></span>
             </div>
             <div class="habilitation-bloc-body collapse" id="habs-Scrum">
-                test
+                <div class="habilitation-body-line">
+                    <span>Accès au contenu</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Ajouter des tâches</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Modifier des tâches</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Supprimer des tâches</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
+    <!-- --------------- KABAN ------------------------->
     <div class="habilitation-bloc col-md-12">
         <div class="habilitation-line">
             <div class="habilitation-bloc-header collapsable" data-toggle="collapse" data-target="#habs-Kaban">
-                Kaban
+                <span><i class="fab fa-trello"></i> Kaban</span>
+                <span class="collapse-arrrow" ><i class="fas fa-chevron-right"></i></span>
             </div>
             <div class="habilitation-bloc-body collapse" id="habs-Kaban">
-                test
+                <div class="habilitation-body-line">
+                    <span>Accès au contenu</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Ajouter des tâches</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Modifier des tâches</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Supprimer des tâches</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
+    <!-- --------------- ISSUES ------------------------->
     <div class="habilitation-bloc col-md-12">
         <div class="habilitation-line">
             <div class="habilitation-bloc-header collapsable" data-toggle="collapse" data-target="#habs-Issues">
-                Issues
+                <span><i class="fas fa-bug"></i> Issues</span>
+                <span class="collapse-arrrow" ><i class="fas fa-chevron-right"></i></span>
             </div>
             <div class="habilitation-bloc-body collapse" id="habs-Issues">
-                test
+                <div class="habilitation-body-line">
+                    <span>Accès au contenu</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Ajouter des issues</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Modifier des issues</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Supprimer des issues</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
+    <!-- --------------- CHAT ------------------------->
     <div class="habilitation-bloc col-md-12">
         <div class="habilitation-line">
             <div class="habilitation-bloc-header collapsable" data-toggle="collapse" data-target="#habs-Chat">
-                Chat
+                <span><i class="fas fa-comment-dots"></i> Chat</span>
+                <span class="collapse-arrrow" ><i class="fas fa-chevron-right"></i></span>
             </div>
             <div class="habilitation-bloc-body collapse" id="habs-Chat">
-                test
+                <div class="habilitation-body-line">
+                    <span>Accès au contenu</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Partager</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Commenter</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
+    <!-- --------------- STATS ------------------------->
     <div class="habilitation-bloc col-md-12">
         <div class="habilitation-line">
             <div class="habilitation-bloc-header collapsable" data-toggle="collapse" data-target="#habs-Stats">
-                Stats
+                <span><i class="fas fa-chart-bar"></i> Statistiques</span>
+                <span class="collapse-arrrow" ><i class="fas fa-chevron-right"></i></span>      
             </div>
             <div class="habilitation-bloc-body collapse" id="habs-Stats">
-                test
+                <div class="habilitation-body-line">
+                    <span>Accès au contenu</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Ajouter des indicateurs</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Modifier des indicateurs</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
+
+                <div class="habilitation-body-line">
+                    <span>Supprimer des indicateurs</span>
+                    <div class="btn-group radio-selector-group" id="status" data-toggle="buttons">
+                        <label class="btn btn-default btn-on btn-xs active">
+                        <input type="radio" value="1" name="multifeatured_module[module_id][status]" checked="checked" class="radio-selector"><i class="fas fa-check"></i></label>
+                        <label class="btn btn-default btn-off btn-xs ">
+                        <input type="radio" value="0" name="multifeatured_module[module_id][status]" class="radio-selector"><i class="fas fa-times"></i></label>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
